aa14eae2254d80a0204947fe50a68f1b91abca77
FcoJavierGlez <FJGS_86@hotmail.com>
2020-12-15 16:26:52 +0100

    Added Chronometer class

---
+++ js/Chronometer.js
@@ -0,0 +1,62 @@
+/**
+ * @author Francisco Javier González Sabariego
+ */
+
+class Chronometer {
+    static #MAX_SECONDS_PER_DAY = 86400;
+
+    #seconds = 0;
+    #idPlay  = 0;
+    #paused  = true;
+
+    constructor(time) {
+        this.#setTime(time);
+    }
+
+    getTime = function() {
+        const [hours,minutes,seconds] = [
+            this.#seconds >= 3600 ? parseInt(this.#seconds/3600) % 60 : 0,
+            this.#seconds >= 60 ? parseInt(this.#seconds/60) % 60 : 0,
+            this.#seconds % 60
+        ];
+        return `${this.#formatTime(hours)}:${this.#formatTime(minutes)}:${this.#formatTime(seconds)}`;
+    }
+
+    togglePause = function() {
+        this.#paused = !this.#paused;
+        this.#paused ? (this.#pause()) : (this.#idPlay = this.#play());
+    }
+
+    /* reset = function() {
+        this.#pause();
+        this.#paused  = true;
+        this.#idPlay  = 0;
+        this.#seconds = 0;
+    } */
+
+    #play = function() {
+        let chrono = this;
+        return setInterval(
+            () => {
+                ++chrono.#seconds;
+                chrono.#seconds %= Chronometer.#MAX_SECONDS_PER_DAY;
+            } , 1000);
+    }
+
+    #pause = () => clearInterval(this.#idPlay);
+
+    #setTime = function(time) {
+        if (isNaN(time))
+            this.#setTimeFromFormat(time);
+        else
+            this.#seconds = time % Chronometer.#MAX_SECONDS_PER_DAY;
+    }
+
+    #setTimeFromFormat = function(time) {
+        const REGEXP_TIME = /^(?<hours>[0-1]\d|2[0-3]):(?<minutes>0\d|[1-5]\d):(?<seconds>0\d|[1-5]\d)$/;
+        const {groups: {hours,minutes,seconds}} = time.match(REGEXP_TIME);
+        this.#seconds = (parseInt(hours) * 3600 + parseInt(minutes) * 60 + parseInt(seconds)) % Chronometer.#MAX_SECONDS_PER_DAY;
+    }
+
+    #formatTime = digit => digit < 10 ? `0${digit}` : digit;
+}
